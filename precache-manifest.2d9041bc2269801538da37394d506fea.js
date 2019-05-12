a078df71cd212b7601f4d519c473690f83009ff1
nhagoober <nguyenphucnha0111@gmail.com>
2019-05-12 11:41:26 +0700

    Updates

---
+++ precache-manifest.2d9041bc2269801538da37394d506fea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41507d98368a1352327d7a65e554a68",
+    "url": "/demotodolist/index.html"
+  },
+  {
+    "revision": "134eb9aaa237f6ab8aa5",
+    "url": "/demotodolist/static/css/main.5b6dc050.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/demotodolist/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "134eb9aaa237f6ab8aa5",
+    "url": "/demotodolist/static/js/main.903caa0d.chunk.js"
+  },
+  {
+    "revision": "ed49ef76239e2b681409",
+    "url": "/demotodolist/static/js/runtime~main.c0beb533.js"
+  }
+]);
